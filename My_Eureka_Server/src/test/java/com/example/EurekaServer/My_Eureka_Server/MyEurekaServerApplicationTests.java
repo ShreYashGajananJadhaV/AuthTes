9d83604492abb18ee06e6064e0ff5ef91462cb4d
Shreyash Gajanan Jadhav <sj2454925@gmail.com>
2024-03-27 14:59:40 +0530

    first commit

---
+++ My_Eureka_Server/src/test/java/com/example/EurekaServer/My_Eureka_Server/MyEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EurekaServer.My_Eureka_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
